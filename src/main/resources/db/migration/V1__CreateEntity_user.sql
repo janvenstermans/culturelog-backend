7e64760f5ba5a96281d2c401b3da646cbbdee492
janv <janvenstermans@gmail.com>
2017-07-29 23:34:54 +0200

    Replace flyway with liquibase

--- src/main/resources/db/migration/V1__CreateEntity_user.sql
+++
@@ -1,8 +0,0 @@
---expected: postgres
-CREATE TABLE culturelog.user
-(
-    id SERIAL PRIMARY KEY,  --SERIAL for psql, others will use bigint and AUTOINCREMENT
-    active boolean NOT NULL,
-    password character varying(255) NOT NULL,
-    username character varying(255) NOT NULL UNIQUE
-)
