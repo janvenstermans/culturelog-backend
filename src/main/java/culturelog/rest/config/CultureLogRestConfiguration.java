75bed0e9cdfcabfe675cdf8e2d4b171742f359b8
janv <janvenstermans@gmail.com>
2017-10-21 22:26:21 +0200

    add locale from header configuration

---
+++ src/main/java/culturelog/rest/config/CultureLogRestConfiguration.java
@@ -0,0 +1,35 @@
+package culturelog.rest.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * @author Jan Venstermans
+ */
+@Configuration
+public class CultureLogRestConfiguration {
+
+    /**
+     * Determine which locale is currently being used, based 1) on the session, 2) cookies, the Accept-Language header, or a fixed value.
+     * @return
+     * @see http://www.baeldung.com/spring-boot-internationalization
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        CultureLogSessionLocaleResolver slr = new CultureLogSessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+//    @Bean
+//    public MessageSource httpErrorFeedbackMessageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("i18n/httpErrorFeedbackMessages");
+//        return messageSource;
+//    }
+}
